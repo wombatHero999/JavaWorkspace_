0ce07f36783404d6469ae1ebf5289e6b3d68f4de
user1 <user1@DESKTOP-H488MRD>
2026-01-09 11:34:25 +0900

    중간커밋

--- 12_IO/src/com/kh/chap04_assist/part02_object/model/dao/ObjectsDao.java
+++ 12_IO/src/com/kh/chap04_assist/part02_object/model/dao/ObjectsDao.java
@@ -1,5 +1,13 @@
 package com.kh.chap04_assist.part02_object.model.dao;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import com.kh.chap04_assist.part02_object.model.vo.Phone;
 
 public class ObjectsDao {
@@ -10,7 +18,42 @@ public class ObjectsDao {
 		arr[1] = new Phone("갤럭시",1000000,"1111");
 		arr[2] = new Phone("갤럭시플립",1200000,"1111");
 		
+		try(ObjectOutputStream oos 
+				= new ObjectOutputStream(new FileOutputStream("phone_arr.txt"));){
+			for(Phone p : arr) {
+				oos.writeObject(p);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	public void fileRead() {
+		
+		try(ObjectInputStream ois 
+				= new ObjectInputStream(new FileInputStream("phone_arr.txt"));){
+			
+			 // 더이상 읽어들일 객체가 없는 경우, EOFException을 발생시킨다.
+			while(true) {
+				Phone p =(Phone) ois.readObject();
+				System.out.println(p);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (EOFException e) {
+			System.out.println("읽을 객체가 없습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+		
+	}
+	
 	
 }
 
--- 12_IO/src/com/kh/chap04_assist/part02_object/model/run/Run.java
+++ 12_IO/src/com/kh/chap04_assist/part02_object/model/run/Run.java
@@ -1,12 +1,18 @@
 package com.kh.chap04_assist.part02_object.model.run;
 
 import com.kh.chap04_assist.part02_object.model.dao.ObjectDao;
+import com.kh.chap04_assist.part02_object.model.dao.ObjectsDao;
 
 public class Run {
 	public static void main(String[] args) {
 		ObjectDao dao = new ObjectDao();
-		dao.fileSave();
-		dao.fileRead();
+		//dao.fileSave();
+		//dao.fileRead();
+		
+		ObjectsDao dao2 = new ObjectsDao();
+		dao2.fileSave();
+		dao2.fileRead();
+		
 	}
 }
 
